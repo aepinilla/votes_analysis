a44f6ba68ce18697bc2c198f35050c2eebffdb10
Andres Pinilla <apinillapalacios@gmail.com>
2022-03-25 11:30:35 +0100

    Benford's law

---
+++ settings.py
@@ -0,0 +1,7 @@
+import os
+
+# Working directory
+d = os.path.dirname(os.getcwd())
+
+# Senado files
+senado_files = list(range(1,7))
--- xls2pkl.py
+++ xls2pkl.py
@@ -3,17 +3,13 @@ import pandas as pd
 from settings import d, senado_files
 
 
-def read_data():
-    senado = []
+def xls2pkl():
     for s in senado_files:
         print('Reading senado ' + str(s))
         file = d + '/votes_analysis/data/excel/senado/senado%s.xlsx' % (s)
-        data = pd.read_pickle(d + '/votes_analysis/data/pickle/senado/senado%s.pkl' % (s))
-        senado.append(data)
-
-    senado_df = pd.concat(senado)
-    return senado_df
+        data = pd.read_excel(file, index_col=0, engine="openpyxl")
+        data.to_pickle(d + '/votes_analysis/data/pickle/senado/senado%s.pkl' % (s))
 
 
 if __name__ == '__main__':
-    read_data()
+    xls2pkl()
